40135e660851cbfbee1b92472bd46ac5509883fe
Manne Tallmarken <mannet@kth.se>
2020-02-10 16:40:06 +0100

    add shell script to start listening for events

---
+++ rtl_mrbeam/mrbeam_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/local/bin/rtl_mrbeam | while read channel; do
+  export tsp="$(date +%s)"
+  curl -X POST -H "Authorization: Bearer ${HASSIO_TOKEN}" -H "Content-Type: application/json" -d "{\"state\": \"${channel}_${tsp}\", \"attributes\":{\"channel\":$channel,\"tsp\":$tsp}}" "http://hassio/homeassistant/api/states/sensor.mrbeam"
+done
