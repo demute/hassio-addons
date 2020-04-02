9702e2588731003723bdc71325f30eec21abd6c1
Manne Tallmarken <mannet@kth.se>
2020-04-02 13:26:03 +0200

    pass gain argument via command line

--- rtl_mrbeam/mrbeam_run.sh
+++ rtl_mrbeam/mrbeam_run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-/usr/local/bin/rtl_mrbeam | while read channel; do
+export LANG=C
+PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+
+CONFIG_PATH=/data/options.json
+GAIN="$(jq --raw-output '.gain' $CONFIG_PATH)"
+echo "MrBeam gain =" $GAIN
+
+/usr/local/bin/rtl_mrbeam -g $GAIN | while read channel; do
   export tsp="$(date +%s)"
   curl -s -X POST -H "Authorization: Bearer ${HASSIO_TOKEN}" -H "Content-Type: application/json" -d "{\"state\": \"${channel}_${tsp}\", \"attributes\":{\"channel\":$channel,\"tsp\":$tsp}}" "http://hassio/homeassistant/api/states/sensor.mrbeam"
 done
